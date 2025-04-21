67a8973c42877fbdf39065e8eb58a2277ec872c3
Kaden Valdez <kvaldez@mines.edu>
2025-04-21 15:46:38 -0600

    Added barChart visual

--- NFLVisuals.py
+++ NFLVisuals.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import random
 import time
+import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import accuracy_score, classification_report
 
@@ -12,8 +13,21 @@ class NFL_Model_Visuals:
     def __init__(self):
         self.NFL_df = pd.read_csv('nfl_data.csv')
 
-        print(self.NFL_df)
 
-NFL_Model_Visuals()
+    def barChart(self):
+        game_results = self.NFL_df[['Team_Full_Name', 'Opp_Full_Name', 'Game_Result']]
+        teams_records = game_results.groupby(['Team_Full_Name', 'Game_Result']).size().unstack(fill_value=0)
+        teams_records = teams_records.rename(columns={'W': 'Wins', 'L': 'Losses', 'T':'Ties'})
+        
+        # Plot
+        teams_records[['Wins', 'Losses', 'Ties']].plot(kind='bar', stacked=True, figsize=(12, 6))
+        plt.title("NFL Team Win/Loss Records")
+        plt.ylabel("Number of Games")
+        plt.xlabel("Team")
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.show()
+
+NFL_Model_Visuals().barChart()
         
     
