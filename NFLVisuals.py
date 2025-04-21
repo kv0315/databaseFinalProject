c908bd6feb78af28fff6bfaf7019e88849dfb5b7
Kaden Valdez <kvaldez@mines.edu>
2025-04-21 15:28:56 -0600

    initialized contructer

--- NFLVisuals.py
+++ NFLVisuals.py
@@ -1,11 +1,19 @@
 # from NFL_Model import *
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+import time
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import accuracy_score, classification_report
 
 
 class NFL_Model_Visuals:
     def __init__(self):
-        None
+        self.NFL_df = pd.read_csv('nfl_data.csv')
+
+        print(self.NFL_df)
+
+NFL_Model_Visuals()
         
     
