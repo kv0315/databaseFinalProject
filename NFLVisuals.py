d37d394504d7f9c5a01e9b8be17a7f223aee2e37
Kaden Valdez <kvaldez@mines.edu>
2025-04-21 15:26:23 -0600

    Added NFLVisuals file to create visuals about out data

---
+++ NFLVisuals.py
@@ -0,0 +1,11 @@
+# from NFL_Model import *
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, classification_report
+
+
+class NFL_Model_Visuals:
+    def __init__(self):
+        None
+        
+    
